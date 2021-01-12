e90d0eff9465118bd839527b66038e749cda7f78
André "decko" de Brito <brito.afa@gmail.com>
2021-01-11 21:52:29 -0300

    Add the new views to the application routes.

--- catservable/core/urls.py
+++ catservable/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CatBreedAPIView
+from .views import CatBreedListAPIView, CatBreedRetrieveAPIView
 
 app_name = "core"
 urlpatterns = [
-    path('breeds/', CatBreedAPIView.as_view(), name="breeds_list")
+    path('breeds/<int:pk>/', CatBreedRetrieveAPIView.as_view(), name="breeds_detail"),
+    path('breeds/', CatBreedListAPIView.as_view(), name="breeds_list")
 ]
