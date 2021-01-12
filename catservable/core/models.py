5a95c6e11abf344eeea9ba6561864ad0957408d6
André "decko" de Brito <brito.afa@gmail.com>
2021-01-11 21:36:41 -0300

    Add CatBreed model and its test.

--- catservable/core/models.py
+++ catservable/core/models.py
@@ -1,3 +1,9 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CatBreed(models.Model):
+    origin = models.CharField(_("Breed Origin"), max_length=60)
+    temperament = ArrayField(models.CharField(_("Temperament"), max_length=50), blank=True, default=list())
+    description = models.TextField(_("Description"))
--- catservable/core/tests.py
+++ catservable/core/tests.py
@@ -1,6 +1,9 @@
+import pytest
 from django.urls import reverse
 
+from .models import CatBreed
 
+pytestmark = pytest.mark.django_db
 
 
 def test_url_to_list_cat_breeds():
@@ -10,3 +13,11 @@ def test_url_to_list_cat_breeds():
     url = reverse(viewname)
 
     assert url == "/breeds/"
+
+
+def test_attributes_for_cat_breed_model():
+
+    attributes = {"origin", "temperament", "description"}
+
+    for attribute in attributes:
+        assert hasattr(CatBreed, attribute)
