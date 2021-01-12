fac05befa020bdb9ca1630a8b141329350a47b74
André "decko" de Brito <brito.afa@gmail.com>
2021-01-11 22:21:02 -0300

    Add new breed fixtures.

--- catservable/core/conftest.py
+++ catservable/core/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from model_bakery import baker
 
+from .models import CatBreed
+
 
 @pytest.fixture
 def catbreed():
@@ -9,3 +11,33 @@ def catbreed():
     yield breed
 
     breed.delete()
+
+
+@pytest.fixture
+def energetic_temper_breed():
+    breed = baker.make("core.CatBreed", temperament=["Energetic"])
+    return breed
+
+
+@pytest.fixture
+def intelligent_temper_breed():
+    breed = baker.make("core.CatBreed", temperament=["Intelligent"])
+    return breed
+
+
+@pytest.fixture
+def independent_temper_breed():
+    breed = baker.make("core.CatBreed", temperament=["Independent"])
+    return breed
+
+
+@pytest.fixture
+def super_breed():
+    breed = baker.make("core.CatBreed", temperament=["Independent", "Energetic"])
+    return breed
+
+
+@pytest.fixture
+def catbreeds(energetic_temper_breed, intelligent_temper_breed, independent_temper_breed, super_breed):
+
+    return CatBreed.objects.all()
