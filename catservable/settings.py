e18b11894409e0bdc51b1aa2791bb8f9455321ea
André "decko" de Brito <brito.afa@gmail.com>
2021-01-11 22:20:37 -0300

    Add django-filters configuration.

--- catservable/settings.py
+++ catservable/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'catservable.core',
 ]
 
@@ -107,3 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]
+}
