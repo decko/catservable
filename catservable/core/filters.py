c44be5853fd64eab9653f81974d68a06ecd9007c
André "decko" de Brito <brito.afa@gmail.com>
2021-01-11 22:22:09 -0300

    Add a new filter to ArrayField.

---
+++ catservable/core/filters.py
@@ -0,0 +1,11 @@
+from django_filters import rest_framework as filters
+
+from .models import CatBreed
+
+
+class ArrayFilter(filters.FilterSet):
+    temperament = filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = CatBreed
+        fields = ("temperament",)
