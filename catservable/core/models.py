812e136be363b29876d20e58eca7dcdfc75641ec
André "decko" de Brito <brito.afa@gmail.com>
2021-01-12 21:25:49 -0300

    Add new fields to CatBreed model.

--- catservable/core/models.py
+++ catservable/core/models.py
@@ -4,6 +4,11 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CatBreed(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(_("Breed Name"), max_length=40, default="")
     origin = models.CharField(_("Breed Origin"), max_length=60)
-    temperament = ArrayField(models.CharField(_("Temperament"), max_length=50), blank=True, default=list())
+    temperament = ArrayField(models.CharField(_("Temperament"), max_length=50), blank=True, default=list)
     description = models.TextField(_("Description"))
+
+    def __str__(self):
+        return f"{self.name} - origin: {self.origin}"
