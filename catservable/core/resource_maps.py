8cf1b42ef01c0a0fddd49c79c001747676aab68c
André "decko" de Brito <brito.afa@gmail.com>
2021-01-17 15:47:44 -0300

    Map images and category endpoints of CatAPI.

--- catservable/core/resource_maps.py
+++ catservable/core/resource_maps.py
@@ -3,5 +3,15 @@ RESOURCE_MAPPING = {
         "resource": "/breeds",
         "docs": "https://docs.thecatapi.com/api-reference/breeds/breeds-list",
         "methods": ["GET"],
-    }
+    },
+    "images": {
+        "resource": "/images/search",
+        "docs": "https://docs.thecatapi.com/api-reference/images/images-search",
+        "methods": ["GET"],
+    },
+    "categories": {
+        "resource": "/categories",
+        "docs": "https://docs.thecatapi.com/api-reference/categories/categories-list",
+        "methods": ["GET"],
+    },
 }
